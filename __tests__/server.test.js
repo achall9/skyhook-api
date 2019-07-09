46305b50fea8c1ba8cd9c70d19dda3d1d60e6dba
Tom Rasmussen <info@tomrasmussen.me>
2019-07-09 17:43:19 -0400

    Initial setup and commit

---
+++ __tests__/server.test.js
@@ -0,0 +1,7 @@
+const request = require('supertest');
+const app = require('../app');
+
+test('GET method should respond with 200', async () => {
+  const response = await request(app).get('/');
+  expect(response.statusCode).toBe(200);
+});
